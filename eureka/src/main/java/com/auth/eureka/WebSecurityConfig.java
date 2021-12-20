a255414a00383310e262917f6097c7cc7ee0caac
rachel5004 <j26597719@naver.com>
2021-12-20 16:40:03 +0900

    [FEAT] WIP add SCG Server

---
+++ eureka/src/main/java/com/auth/eureka/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.auth.eureka;
+
+//@EnableWebSecurity
+//class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf()
+//                .disable()
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic();
+//    }
+//}
